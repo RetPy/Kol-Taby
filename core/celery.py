6cf490744b3102dc4829851b5f8e37b6a46801c5
RetPy <shad02105@gmail.com>
2022-06-02 19:50:56 +0600

    hashed celery

--- core/celery.py
+++ core/celery.py
@@ -1,19 +1,19 @@
-import os
-
-from celery import Celery
-from celery.schedules import crontab
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
-
-app = Celery('core')
-
-app.config_from_object('django.conf:settings', namespace='CELERY')
-app.autodiscover_tasks()
-
-app.conf.beat_schedule = {
-    'auto-delete-every-minute': {
-        'task': 'core.tasks.auto_delete',
-        'schedule': crontab(minute=0, hour=0),
-    }
-}
-app.conf.timezone = 'Asia/Bishkek'
+# import os
+#
+# from celery import Celery
+# from celery.schedules import crontab
+#
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+#
+# app = Celery('core')
+#
+# app.config_from_object('django.conf:settings', namespace='CELERY')
+# app.autodiscover_tasks()
+#
+# app.conf.beat_schedule = {
+#     'auto-delete-every-minute': {
+#         'task': 'core.tasks.auto_delete',
+#         'schedule': crontab(minute=0, hour=0),
+#     }
+# }
+# app.conf.timezone = 'Asia/Bishkek'
--- core/tasks.py
+++ core/tasks.py
@@ -1,12 +1,12 @@
-from datetime import datetime
-
-from core.celery import app
-from apps.todos.models import Todo
-
-
-@app.task()
-def auto_delete():
-    for todo in Todo.objects.filter(status='4'):
-        res = todo.deadline.date() - datetime.date(datetime.today())
-        if res.days <= -8:
-            todo.delete()
+# from datetime import datetime
+#
+# from core.celery import app
+# from apps.todos.models import Todo
+#
+#
+# @app.task()
+# def auto_delete():
+#     for todo in Todo.objects.filter(status='4'):
+#         res = todo.deadline.date() - datetime.date(datetime.today())
+#         if res.days <= -8:
+#             todo.delete()
