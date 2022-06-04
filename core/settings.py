6fb82e7bdb97b3b97dd69a39023c2d8cd819a052
RetPy <shad02105@gmail.com>
2022-06-04 20:59:40 +0600

    pagination 1 mil page_size

--- core/settings.py
+++ core/settings.py
@@ -113,7 +113,7 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.AllowAny',
     ),
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 1000000,
 }
 
