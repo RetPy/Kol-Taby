a9e8db08b5b397e287eb563fe3f962dbd9e7dfb9
RetPy <shad02105@gmail.com>
2022-06-14 17:01:52 +0600

    changing pagination class

--- core/settings.py
+++ core/settings.py
@@ -113,7 +113,7 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.AllowAny',
     ),
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
     'PAGE_SIZE': 1000000,
 }
 
